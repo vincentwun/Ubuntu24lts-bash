802330fec07f996f65ece5bbcc9db25c01130cc5
vincentwun <144003077+vincentwun@users.noreply.github.com>
2025-10-10 11:44:54 +0800

    updated devops.sh

--- devops.sh
+++ devops.sh
@@ -11,6 +11,9 @@ wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sh
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 
+# Enable bash completion for Terraform
+terraform -install-autocomplete
+
 # Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
